60541f0c451d553512e285f90472bd78e1f6f89b
Isak Lundmark <isaklund@lycksele.se>
2018-11-30 11:16:17 +0100

    Worked on the big toDo

--- projects/toDo/assets/js/toDo.js
+++ projects/toDo/assets/js/toDo.js
@@ -1,5 +1,32 @@
 // To Doooooo
 
-var array = [];
 
-document.getElementById("button").addEventListener("click")
+
+document.getElementById("button").addEventListener("click", toDo)
+
+function toDo(array) {
+    var array = [];
+    var input = 0;
+    while(input != "q") {
+        input = prompt("Vad vill du göra?");
+
+        if(input == "new") {
+            input = prompt("Vad vill du komma ihåg?");
+            array.push(input);
+        }
+
+        else if(input == "ls") {
+            for(var i = 0; i <= array.length-1; i++) {
+                console.log("["+ i + "]  " + array[i])
+            }
+        }
+
+        else if(input == "del") {
+            input = prompt("Vilken vill du ta bort?");
+            array.splice(input , 1);
+        }
+    }
+
+
+
+}
