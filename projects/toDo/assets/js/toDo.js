8856a77629ede8701faeac8bfc933f88e37dfc30
Isak Lundmark <isaklund@lycksele.se>
2018-12-06 14:01:10 +0100

    Worked on the toDO

--- projects/toDo/assets/js/toDo.js
+++ projects/toDo/assets/js/toDo.js
@@ -1,32 +1,39 @@
 // To Doooooo
+var array = [];
 
+document.getElementById("button").addEventListener("click", toDo);
 
-
-document.getElementById("button").addEventListener("click", toDo)
-
-function toDo(array) {
-    var array = [];
+function toDo() {
     var input = 0;
     while(input != "q") {
         input = prompt("Vad vill du göra?");
 
         if(input == "new") {
             input = prompt("Vad vill du komma ihåg?");
+            console.log("Saved " + "'" + input + "'" +" to the to do list.");
             array.push(input);
         }
 
         else if(input == "ls") {
+            console.log("------------------------------");
             for(var i = 0; i <= array.length-1; i++) {
-                console.log("["+ i + "]  " + array[i])
+                console.log("[" + i + "]  " + array[i])
             }
+            console.log("------------------------------");
         }
 
         else if(input == "del") {
             input = prompt("Vilken vill du ta bort?");
+            console.log("Removed " + "'" + array[input] + "'" + " from the list.");
             array.splice(input , 1);
         }
+
+        else if(input == "q") {
+            console.log("Shutting down...")
+        }
+
+        else {
+            console.log("Please insert a valid command.")
+        }
     }
-
-
-
 }
