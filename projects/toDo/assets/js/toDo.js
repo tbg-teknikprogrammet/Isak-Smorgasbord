9653ea14e340af3745b5cbb8e5aacd3b397c9a70
Isak Lundmark <isaklund@lycksele.se>
2018-12-07 11:14:34 +0100

    Made comments on the toDo

--- projects/toDo/assets/js/toDo.js
+++ projects/toDo/assets/js/toDo.js
@@ -1,31 +1,31 @@
 // To Doooooo
 var array = [];
 
-document.getElementById("button").addEventListener("click", toDo);
+document.getElementById("button").addEventListener("click", toDo); // Starts the toDO function when the button is clicked.
 
 function toDo() {
     var input = 0;
     while(input != "q") {
-        input = prompt("Vad vill du göra?");
+        input = prompt("Please insert a command.");
 
-        if(input == "new") {
-            input = prompt("Vad vill du komma ihåg?");
-            console.log("Saved " + "'" + input + "'" +" to the to do list.");
-            array.push(input);
+        if(input == "new") { 
+            input = prompt("What do you want to remember?");
+            console.log("Added " + '"' + input + '"' +" to the to do list.");
+            array.push(input); // Adds a new value to the last part of the list.
         }
 
         else if(input == "ls") {
             console.log("------------------------------");
-            for(var i = 0; i <= array.length-1; i++) {
+            for(var i = 0; i <= array.length-1; i++) { // Loops through the list and put the values individually indexed in the console.
                 console.log("[" + i + "]  " + array[i])
             }
             console.log("------------------------------");
         }
 
         else if(input == "del") {
-            input = prompt("Vilken vill du ta bort?");
-            console.log("Removed " + "'" + array[input] + "'" + " from the list.");
-            array.splice(input , 1);
+            input = prompt("Which indexed value do you want to delet?");
+            console.log("Removed " + '"' + array[input] + '"' + " from the list.");
+            array.splice(input , 1); // Removes an indexed value.
         }
 
         else if(input == "q") {
