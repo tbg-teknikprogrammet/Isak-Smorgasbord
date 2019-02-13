3b83a9f350e5e42070e091b643a07df8fe611ec3
Isak Lundmark <isaklund@lycksele.se>
2019-02-13 11:21:12 +0100

    Did more DOM

--- projects/javascript101/assets/js/11-DOM-3-manipulate.js
+++ projects/javascript101/assets/js/11-DOM-3-manipulate.js
@@ -15,3 +15,23 @@ var spc = document.getElementsByClassName("special")[0];
 spc.classList.add("another-class");
 // spc.classList.remove("another-class");
 // spc.classList.toggle("another-class");
+
+
+
+// manipulating text and content
+var pEle = document.querySelector(".test");
+pEle.textContent = "This is an big <strong>awsome</strong> paragraph"
+pEle.innerHTML = "This is an bigererer <strong>awsome</strong> paragraph"
+
+// Chaining methods
+document.getElementsByTagName("h1")[1].textContent = "Yeetus maneetus badeetus";
+
+// Manipulating attributes
+var link = document.querySelector("a")
+link.getAttribute("href");
+link.setAttribute("href", "http://teknikundervisning.se/")
+
+document.getElementsByTagName("img")[0].setAttribute("src", "https://pbs.twimg.com/profile_images/903598038303866882/E1T6vXVj_400x400.jpg")
+
+
+
---
+++ projects/javascript101/assets/js/12-DOM-4-events.js
@@ -0,0 +1,27 @@
+// Events
+// select the button
+
+var btn = document.querySelector("button")
+
+// manipulate
+btn.addEventListener("click", function() {
+    document.querySelector("p").innerHTML = "Yeetus I gotus the clickus"
+});
+
+
+var lis = document.querySelectorAll("li");
+
+for(var i = 0; i < lis.length; i++) {
+    lis[i].addEventListener("click", function(){
+        this.style.textDecoration = "line-through";
+    });
+}
+
+// color changer
+
+var btnToggle = document.querySelector("#btnToggleBg");
+ btnToggle.addEventListener("click", function(){
+    document.getElementsByTagName("body")[0].classList.toggle("toggleBg")
+ });
+
+ 
