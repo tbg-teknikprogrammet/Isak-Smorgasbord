efb310cf3d4a7ff3037bf29014a49b3592b9e4b8
Isak Lundmark <isaklund@lycksele.se>
2019-03-20 11:22:56 +0100

    Started the on Slideshow

--- projects/js-playground/assets/js/play.js
+++ projects/js-playground/assets/js/play.js
@@ -1,4 +1,32 @@
-
+// Lightbox
 document.querySelector("#dab").addEventListener("click", function(){
     document.querySelector("#light").style = "display:block;"
 });
+
+document.querySelector("#light").addEventListener("click", function(){
+    document.querySelector("#light").style = "display:none;"
+});
+
+// SLIDESHOW
+var slideShow = ["assets/images/Dabmeister.jpg", "assets/images/Snke.jpg", "assets/images/Xpoke.jpg"]
+var slideIndex = 1;
+document.querySelector("#slide img").src=slideShow[slideIndex];
+
+document.querySelector("#prev").addEventListener("click", function(){
+    slideIndex += -1;
+    if(slideIndex < 0 ) {
+        slideIndex = slideShow.length;
+    }
+    document.querySelector("#slide img").src=slideShow[slideIndex];
+});
+
+document.querySelector("#next").addEventListener("click", function(){
+    slideIndex += 1;
+    if(slideIndex > slideShow.length-1) {
+        slideIndex = 0;
+    }
+    document.querySelector("#slide img").src=slideShow[slideIndex];
+});
+
+
+
